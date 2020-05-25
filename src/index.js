7d79ff0b570fbcdd2d0980c482b88a1489118542
iida nozomi <nozomi@mbp.local>
2020-05-25 21:39:13 +0900

    ファイル名の変更

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import About from './components/HomeAbout';
-import Attractive from './components/Attractive';
-import Firstview from './components/Firstview';
+import Attractive from './components/HomeAttractive';
+import Firstview from './components/HomeFirstview';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import LINE from './components/LINE';
-import Movie from './components/Movie';
-import Place from './components/Place';
+import Movie from './components/HomeMovie';
+import Place from './components/HomePlace';
 import './styles/main.scss';
 
 
