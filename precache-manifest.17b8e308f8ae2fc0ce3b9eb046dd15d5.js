38417d22737f3bb4b5827862259b1a6a30e50ed3
iida nozomi <nozomi@mbp.local>
2020-05-25 10:27:20 +0900

    Updates

---
+++ precache-manifest.17b8e308f8ae2fc0ce3b9eb046dd15d5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b49d6332509d582005a8f920ff5f9da",
+    "url": "/sting-hp/index.html"
+  },
+  {
+    "revision": "68d892f2bf8fc42083c5",
+    "url": "/sting-hp/static/css/main.72f7858f.chunk.css"
+  },
+  {
+    "revision": "94822d8e3f8fbd7e5671",
+    "url": "/sting-hp/static/js/2.b4739d8c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/sting-hp/static/js/2.b4739d8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68d892f2bf8fc42083c5",
+    "url": "/sting-hp/static/js/main.8f14a029.chunk.js"
+  },
+  {
+    "revision": "c5ebc7af9d97d1c10a91",
+    "url": "/sting-hp/static/js/runtime-main.7d2824f6.js"
+  },
+  {
+    "revision": "a5dda792679b812ecc85c533829a8e70",
+    "url": "/sting-hp/static/media/IMG_8922.a5dda792.JPG"
+  },
+  {
+    "revision": "8b11f859627ca8ccd30b940e73adf6b8",
+    "url": "/sting-hp/static/media/Place.8b11f859.jpg"
+  },
+  {
+    "revision": "df0685305583804bbc8217e77d342399",
+    "url": "/sting-hp/static/media/sting-movie.df068530.mp4"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/sting-hp/precache-manifest.17b8e308f8ae2fc0ce3b9eb046dd15d5.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sting-hp/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
